d1623a9e731b9b25f5cd1337353baa8b3c48408f
Nischal Bhattarai <nishchalb21@gmail.com>
2025-02-27 21:26:37 +0545

    feat: initialize server with Express and add basic API endpoint

---
+++ server/app.js
@@ -0,0 +1,14 @@
+import express from "express";
+
+
+const app = express();
+
+
+
+app.use('/api/test' ,(req,res) =>{
+    res.send("hello world");
+})
+
+app.listen(8800, () =>{
+    console.log("Server is running !");
+})
